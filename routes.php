2f90b5837fe29023d7bee2e21b8b0c31ed987ad3
Szabó Gergő <gergo85@gmail.com>
2015-04-28 09:49:02 +0200

    Minor changes

--- routes.php
+++ routes.php
@@ -6,7 +6,7 @@ App::before(function($request)
         Route::any('indikator/qedit/content', function() {
             if (File::exists('themes/'.get('path')))
             {
-                $content = file_get_contents('themes/'.get('path'));
+                $content = File::get('themes/'.get('path'));
                 if (substr_count(get('path'), '/content/') == 0) $content = substr($content, strrpos($content, '==') + 2);
                 return trim($content);
             }
@@ -20,7 +20,7 @@ App::before(function($request)
         Route::any('indikator/qedit/date', function() {
             if (File::exists('themes/'.get('path')))
             {
-                return date('Y-m-d G:i', filemtime('themes/'.get('path')));
+                return date('Y-m-d G:i', File::lastModified('themes/'.get('path')));
             }
 
             else
