f6ac90e492f2c80eded08394a5faedd91cdf3fe4
Szabó Gergő <gergo85@gmail.com>
2016-06-24 09:27:59 +0200

    Updated for latest October

--- routes.php
+++ routes.php
@@ -2,10 +2,13 @@
 
 App::before(function($request)
 {
-    Route::group(['prefix' => Config::get('cms.backendUri', 'backend')], function() {
-        Route::any('indikator/qedit/content', function() {
+    Route::group(['prefix' => Config::get('cms.backendUri', 'backend')], function()
+    {
+        Route::any('indikator/qedit/content', function()
+        {
             if (File::exists('themes/'.get('path'))) {
                 $content = File::get('themes/'.get('path'));
+
                 if (substr_count(get('path'), '/content/') == 0) {
                     $content = substr($content, strrpos($content, '==') + 2);
                 }
@@ -18,17 +21,12 @@ App::before(function($request)
             }
         });
 
-        Route::any('indikator/qedit/date', function() {
+        Route::any('indikator/qedit/date', function()
+        {
             if (File::exists('themes/'.get('path'))) {
                 $modified = File::lastModified('themes/'.get('path'));
 
-                $date = IntlDateFormatter::create(
-                    App::getLocale(),
-                    IntlDateFormatter::MEDIUM,
-                    IntlDateFormatter::NONE
-                )->format($modified);
-
-                return date($date.' G:i', $modified);
+                return date('Y-m-d G:i', $modified);
             }
 
             else {
